0b3a7766ce48da7c17173dfb944b85f14e821b11
Kakachu <kauajardim2004@hotmail.com>
2023-07-12 17:39:28 -0300

    Testing Command

---
+++ Projects/PaymentContext/PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PaymentContext.Domain.Commands
+{
+	[TestClass]
+    public class MyTestClass
+    {
+        [TestMethod]
+        public void ShouldReturnErrorWhenNameIsInvalid()
+        {
+            var command = new CreateBoletoSubscriptionCommand();
+            command.FirstName = "";
+
+            command.Validate();
+            Assert.AreEqual(false, command.IsValid);
+        }
+    }
+}
