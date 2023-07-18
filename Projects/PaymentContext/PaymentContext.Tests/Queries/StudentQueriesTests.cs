d8a58aca55e4f1dd7fc100da643c12d93434c6f9
Kakachu <kauajardim2004@hotmail.com>
2023-07-17 23:53:59 -0300

    Queries

---
+++ Projects/PaymentContext/PaymentContext.Tests/Queries/StudentQueriesTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enum;
+using PaymentContext.Domain.Queries;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests.Queries
+{
+	[TestClass]
+	public class StudentQueriesTests
+	{
+		private IList<Student> _students;
+
+        public StudentQueriesTests()
+        {
+			for (var i = 0; i <= 10; i++)
+			{
+				_students.Add(new Student(
+					new Name("student", i.ToString()),
+					new Identification("11111111111" + i.ToString(), EIdentificationType.CPF),
+					new Email("student.one@gmail.com")
+					));
+			}
+        }
+
+        [TestMethod]
+		public void ShouldReturnNullWhenIdentificationNotExists()
+		{
+			var expression = StudentQueries.GetStudent("12345678911");
+			var student = _students.AsQueryable().Where(expression).FirstOrDefault();
+
+			Assert.AreEqual(null, student);
+		}
+	}
+}
